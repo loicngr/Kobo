c92eeb826461ad1e57aa4b5c641694a486cae19a
loicngr <loicnogier@ik.me>
2026-04-10 21:46:08 +0200

    feat(templates): fetch templates once at app mount

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import { useWebSocketStore } from 'src/stores/websocket'
+import { useTemplatesStore } from 'src/stores/templates'
 import { requestNotificationPermission } from 'src/utils/notifications'
 import { onMounted, onUnmounted } from 'vue'
 
 const wsStore = useWebSocketStore()
+const templatesStore = useTemplatesStore()
 
 onMounted(() => {
   wsStore.connect()
+  templatesStore.fetchTemplates()
   requestNotificationPermission()
 })
 
